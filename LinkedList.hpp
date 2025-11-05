f78508d529ead067c7ba4bec7b82d8adc9726e14
Aryan Thakur <52215513+importly@users.noreply.github.com>
2025-11-04 22:11:22 -0500

    new linked list stuff

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -53,19 +53,80 @@ public:
 	};
 
 	// Removal
-	bool removeHead();
-	bool removeTail();
-	void Clear();
+	bool removeHead() {
+		if (head->next==nullptr) return false;
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+		count--;
+		return true;
+	};
+	bool removeTail() {
+		if (head->prev==nullptr) return false;
+		Node* temp = tail;
+		tail = tail->prev;
+		delete temp;
+		count--;
+		return true;
+	};
+	void Clear() {
+		Node* deleteing = head;
+		Node* next = nullptr;
+		while (deleteing) {
+			next = deleteing->next;
+			delete deleteing;
+			deleteing = next;
+		}
+		count = 0;
+		head = nullptr;
+		tail = nullptr;
+	};
 
 	// Operators
-	LinkedList<T>& operator=(LinkedList<T>&& other) noexcept;
-	LinkedList<T>& operator=(const LinkedList<T>& rhs);
+	LinkedList<T>& operator=(LinkedList<T>&& rhs) noexcept { //move
+		if (&rhs == this) return *this;
+		Clear();
+
+		head = rhs.head;
+		tail = rhs.tail;
+		count = rhs.count;
+
+		rhs.head = nullptr;
+		rhs.tail = nullptr;
+		rhs.count = 0;
+
+		return *this;
+	};
+	LinkedList<T>& operator=(const LinkedList<T>& rhs) { // copy
+		if (&rhs == this) return *this;
+		Clear();
+
+		Node* current = rhs.head;
+		while (current != nullptr) {
+			addTail(current->data);
+			current = current->next;
+		}
+
+		return *this;
+	};
 
 	// Construction/Destruction
-	LinkedList();
-	LinkedList(const LinkedList<T>& list);
-	LinkedList(LinkedList<T>&& other) noexcept;
-	~LinkedList();
+	LinkedList(): head(nullptr), tail(nullptr), count(0) {};
+	LinkedList(const LinkedList<T>& list): head(nullptr), tail(nullptr), count(0) {
+		Node* current = list.head;
+		while (current != nullptr) {
+			addTail(current->data);
+			current = current->next;
+		};
+	}
+	LinkedList(LinkedList<T>&& other) noexcept: head(other.head), tail(other.tail), count(other.count)  {
+		other.head = nullptr;
+		other.tail = nullptr;
+		other.count = 0;
+	};
+	~LinkedList() {
+		Clear();
+	};
 
 private:
 	// Stores pointers to first and last nodes and count
