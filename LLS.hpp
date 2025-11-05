eaba66052bf969345f1ea98ead9953a09b791bd0
Aryan Thakur <52215513+importly@users.noreply.github.com>
2025-11-05 01:33:09 -0500

    linked stack

--- LLS.hpp
+++ LLS.hpp
@@ -11,17 +11,25 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLS();
+    LLS() : list() {};
 
     // Insertion
-    void push(const T& item) override;
+    void push(const T& item) override {
+        list.addTail(item);
+    };
 
     // Deletion
-    T pop() override;
+    T pop() override{
+        T item{list.tail->data};
+        list.removeTail();
+        return item;
+    };
 
     // Access
-    T peek() const override;
+    T peek() const override {return list.tail->data;};
 
     //Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    };
 };
