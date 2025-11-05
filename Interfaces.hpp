b97770ab87fe784fc29adee53e4013764e3aa4e4
Aryan Thakur <52215513+importly@users.noreply.github.com>
2025-11-04 21:54:05 -0500

    ez interfaces

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -4,20 +4,34 @@
 #include <stdexcept>
 
 
-template <typename T>
+template<typename T>
 class StackInterface {
-
+    virtual ~StackInterface() = 0;
+    [[nodiscard]] virtual std::size_t getSize() const noexcept = 0;
+    virtual void push(const T &data) = 0;
+    virtual T peek() const = 0;
+    virtual T pop() = 0;
 };
 
 
-template <typename T>
+template<typename T>
 class QueueInterface {
-
+    virtual ~QueueInterface() = 0;
+    [[nodiscard]] virtual std::size_t getSize() const noexcept = 0;
+    virtual void enqueue(const T& data)=0;
+    virtual T peek() const=0;
+    virtual T dequeue()=0;
 };
 
 
-template <typename T>
+template<typename T>
 class DequeInterface {
-
+    virtual ~DequeInterface() = 0;
+    virtual void pushFront(const T& item) = 0;
+    virtual void pushBack(const T& item) = 0;
+    virtual T popFront() = 0;
+    virtual T popBack() = 0;
+    virtual const T& front() const = 0;
+    virtual const T& back() const = 0;
+    [[nodiscard]] virtual  std::size_t getSize() const noexcept = 0;
 };
-
